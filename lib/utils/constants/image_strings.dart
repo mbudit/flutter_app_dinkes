dc7d1a057a3bdd0c8d8268f9c9a3f3f7baa39726
M. Budi Triono <buditriono13@gmail.com>
2024-05-11 20:58:23 +0700

    nambahin components di folder constants

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TImages {
+  TImages._();
+
+  // Sebagian besar dari sini
+  // merujuk ke folder assets semua
+  // jadi direktorinya ke situ yaa
+  // kalo mau nambah logo dsb, taronya di folder assets.
+
+  // Logo Aplikasi
+  static const String lightAppLogo = "";
+  static const String darkAppLogo = "";
+
+  // Logo bersifat yang kenegaraan kek instansi, pancasila dll
+  static const String logoPancasila = "";
+
+}
